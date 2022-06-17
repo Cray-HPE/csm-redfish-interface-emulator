c7c90560ec6e3446314ebeb044a1bdb9f5b26708
Manny Alvarez III <manuel.alvarez@hpe.com>
2022-06-17 09:37:06 -0500

    Add an XL675d mockup with A100 GPUs
    Description:
    This adds the XL675d_A100 mockup to RIE and fixes resource path handling so trailing and double slashs are normalized.

--- src/api_emulator/resource_dictionary.py
+++ src/api_emulator/resource_dictionary.py
@@ -1,11 +1,42 @@
 # Copyright Notice:
 # Copyright 2016-2019 DMTF. All rights reserved.
 # License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Interface-Emulator/blob/master/LICENSE.md
+#
+# The original DMTF contents of this file have been modified to support
+# The CSM Redfish Interface Emulator. These modifications are subject to the following:
+# Copyright 2022 Hewlett Packard Enterprise Development LP
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from this
+# software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 # Resource Dictionary
 
 # Variable to store resource dictionary
 import logging
+import os.path
 
 resdict = {}
 
@@ -16,18 +47,34 @@ class ResourceDictionary(object):
 
 
     def get_resource(self, path):
-        obj = resdict[path].configuration
+        if path != '':
+            p = os.path.normpath(path)
+        else:
+            p = path
+        obj = resdict[p].configuration
         return obj
 
     def get_object(self, path):
-        return resdict[path]
+        if path != '':
+            p = os.path.normpath(path)
+        else:
+            p = path
+        return resdict[p]
 
     def add_resource(self, path, obj):
-        resdict[path] = obj
+        if path != '':
+            p = os.path.normpath(path)
+        else:
+            p = path
+        resdict[p] = obj
         return obj
 
     def delete_resource(self, path):
-        del resdict[path]
+        if path != '':
+            p = os.path.normpath(path)
+        else:
+            p = path
+        del resdict[p]
 
     def print_dictionary(self):
         for x in resdict:
